91ef298a3234a0800528a338f0ac7317d26e27e4
Senesh Nagodavithana <seneshnagodavithana@gmail.com>
2025-03-02 23:00:32 +0530

    frontend start

---
+++ cartproject-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import AppRoutes from "./routes/AppRoutes";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <AppRoutes />
+    </>
+  );
+}
+
+export default App;
---
+++ cartproject-frontend/src/pages/homePage/HomePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomePage() {
+  return <div>Hom</div>;
+}
+
+export default HomePage;
